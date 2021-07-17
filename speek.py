a87960290b1a855656301f00d79bf1683fc19693
ifgeniy <jon_vy@mail.ru>
2021-07-17 20:52:55 +0300

    Кнопка записать mp3 работает

--- speek.py
+++ speek.py
@@ -1,4 +1,5 @@
 import pyttsx3  # Для озвучивания и записи
+import glob
 import os
 
 
@@ -10,10 +11,11 @@ eng_voice_id = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_M
 # <editor-fold desc="file_name">
 def file_name():
     path = f"{os.environ['PYTHONPATH']}\\audio"  # Путь к директории аудио
-    file_name = len(os.listdir(path)) + 1  # Количество файлов в этой директории
+    file_name = len(glob.glob(f'{path}\*.mp3')) + 1  # Количество файлов в этой директории
     return file_name
 
 # </editor-fold>
+
 # <editor-fold desc="Функция воспроизведения">
 '''Принимает текст, 
 скорость воспроизведения,
@@ -24,7 +26,8 @@ def sp(txt, rate, voice_id):
     engine.say(txt)
     engine.runAndWait()
 # </editor-fold>
-# <editor-fold desc="Функция записи текста в mp3">
+
+# <editor-fold desc="Функция записи текста в mp3. Так же формирует txt">
 '''Принимает текст, 
 имя файла, 
 id голосового движка установленного в системе, 
@@ -35,6 +38,14 @@ def wr(txt, rate, voice_id, lang_file):
     engine.setProperty('voice', voice_id)
     engine.save_to_file(txt, f'audio/{lang_file}_{count}.mp3')
     engine.runAndWait()
+    with open("audio/file.txt", "a") as file:
+        file.write(f'{lang_file}_{count}.mp3\n')
+# </editor-fold>
+
+# <editor-fold desc="добавляет в список имена файлов">
+def write_file_name(name_mp3, lang_file):
+    with open("file.txt", "a") as file:
+        file.write(f'{name_mp3}.mp3\n')
 # </editor-fold>
 
 if __name__ == "__main__":
